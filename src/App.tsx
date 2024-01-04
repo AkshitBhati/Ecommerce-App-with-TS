4c39dd85bb770b8a9deb55c57ba1079645346967
Hunarship <149655393+Hunarship@users.noreply.github.com>
2024-01-04 23:00:13 +0530

    setup to the project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { lazy, Suspense } from "react"
+import Loader from "./Components/Loader"
+
+//implementing lazy loading in the app
+const HomePage = lazy(() => import("./Pages/HomePage"))
+const SearchPage = lazy(() => import("./Pages/SearchPage"))
+const CartPage = lazy(() => import("./Pages/CartPage"))
+
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+   <Router>
+    <Suspense fallback={<Loader />}>
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/search" element={<SearchPage />}/>
+      <Route path="/cart" element={<CartPage />}/>
+
+    </Routes>
+    </Suspense>
+   </Router>
   )
 }
 
